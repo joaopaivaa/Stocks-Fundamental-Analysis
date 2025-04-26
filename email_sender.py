826cdba189d81c198f87abd5ef0baa809a4aff47
João Vitor de Paiva Marcotti <joaovmarcotti@hotmail.com>
2025-04-26 13:51:32 -0300

    creates email sender

---
+++ email_sender.py
@@ -0,0 +1,39 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+import os
+from analysis_brazil import df_evaluated as df_evaluated_brazil
+from analysis_us import df_evaluated as df_evaluated_us
+import time
+
+start = time.time()
+
+smtp_server = "smtp.gmail.com"
+smtp_port = 587
+email = "joaopaiva.raspberrypi@gmail.com"
+password = os.getenv("RASPBERRY_PASSWORD")
+
+destinatario = "joaovmarcotti@hotmail.com"
+assunto = "Stocks List"
+mensagem = f"Brazil Stocks List\n{df_evaluated_brazil[['ticker', 'Portfolio %', 'Portfolio Value']].to_string(index=False)}\n\nUS Stocks List\n{df_evaluated_us[['ticker', 'Portfolio %', 'Portfolio Value']].to_string(index=False)}"
+
+msg = MIMEMultipart()
+msg['From'] = email
+msg['To'] = destinatario
+msg['Subject'] = assunto
+msg.attach(MIMEText(mensagem, 'plain'))
+
+try:
+    server = smtplib.SMTP(smtp_server, smtp_port)
+    server.starttls()
+    server.login(email, password)
+    server.sendmail(email, destinatario, msg.as_string())
+    print("E-mail sent!")
+except Exception as e:
+    print(f"Error: {e}")
+finally:
+    server.quit()
+
+end = time.time()
+
+print(f"Execution time: {end - start:.2f} seconds")
