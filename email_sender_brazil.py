14409949e43092890d6983ef68a9102f7d07d762
João Vitor de Paiva Marcotti <joaovmarcotti@hotmail.com>
2025-04-28 19:05:47 -0300

    executes just in the 10th day of the month

--- email_sender_brazil.py
+++ email_sender_brazil.py
@@ -1,39 +1,48 @@
-import time
+from datetime import datetime
 
-start = time.time()
+today = datetime.now()
 
-import smtplib
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
-import os
-from analysis_brazil import df_evaluated as df_evaluated_brazil
+if today.day == 10:
+    
+    import time
 
-smtp_server = "smtp.gmail.com"
-smtp_port = 587
-email = "joaopaiva.raspberrypi@gmail.com"
-password = os.getenv("RASPBERRY_PASSWORD")
+    start = time.time()
 
-destinatario = "joaovmarcotti@hotmail.com"
-assunto = "Brazil Stocks List"
-mensagem = f"Brazil Stocks List\n{df_evaluated_brazil[['ticker', 'name', 'Portfolio %', 'Portfolio Value']].to_string(index=False)}"
+    import smtplib
+    from email.mime.text import MIMEText
+    from email.mime.multipart import MIMEMultipart
+    import os
+    from analysis_brazil import df_evaluated as df_evaluated_brazil
 
-msg = MIMEMultipart()
-msg['From'] = email
-msg['To'] = destinatario
-msg['Subject'] = assunto
-msg.attach(MIMEText(mensagem, 'plain'))
+    smtp_server = "smtp.gmail.com"
+    smtp_port = 587
+    email = "joaopaiva.raspberrypi@gmail.com"
+    password = os.getenv("RASPBERRY_PASSWORD")
 
-try:
-    server = smtplib.SMTP(smtp_server, smtp_port)
-    server.starttls()
-    server.login(email, password)
-    server.sendmail(email, destinatario, msg.as_string())
-    print("E-mail sent!")
-except Exception as e:
-    print(f"Error: {e}")
-finally:
-    server.quit()
+    destinatario = "joaovmarcotti@hotmail.com"
+    assunto = "Brazil Stocks List"
+    mensagem = f"Brazil Stocks List\n{df_evaluated_brazil[['ticker', 'name', 'Portfolio %', 'Portfolio Value']].to_string(index=False)}"
 
-end = time.time()
+    msg = MIMEMultipart()
+    msg['From'] = email
+    msg['To'] = destinatario
+    msg['Subject'] = assunto
+    msg.attach(MIMEText(mensagem, 'plain'))
 
-print(f"Execution time: {end - start:.2f} seconds")
+    try:
+        server = smtplib.SMTP(smtp_server, smtp_port)
+        server.starttls()
+        server.login(email, password)
+        server.sendmail(email, destinatario, msg.as_string())
+        print("E-mail sent!")
+    except Exception as e:
+        print(f"Error: {e}")
+    finally:
+        server.quit()
+
+    end = time.time()
+
+    print(f"Execution time: {end - start:.2f} seconds")
+
+else:
+    print("Today is not the 10th of the month. No email will be sent.")
