2544e5e2c32e77ea0dac24cd2edbe985efd35dcf
João Vitor de Paiva Marcotti <joaovmarcotti@hotmail.com>
2025-05-25 17:42:52 -0300

    saves past portfolios

--- analysis_us.py
+++ analysis_us.py
@@ -1,3 +1,6 @@
+import pandas as pd
+from datetime import datetime
+
 from query_US import grades_df, stocks_df
 
 df_grades = grades_df.copy()
@@ -18,3 +21,9 @@ df_evaluated['Portfolio %'] = round(100 * df_evaluated['Grade'] / df_evaluated['
 
 portfolio_total_value = 1000
 df_evaluated['Portfolio Value'] = round(df_evaluated['Portfolio %'] * portfolio_total_value / 100, 2)
+
+df_evaluated.to_csv('df_evaluated_us.csv', index=False)
+
+past_portfolios = pd.read_csv('past_portfolios_us.csv')
+past_portfolios[f"{datetime.now().strftime('%Y-%m-%d')}"] = df_evaluated['ticker']
+past_portfolios.to_csv('past_portfolios_us.csv', index=False)
